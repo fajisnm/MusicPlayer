35e1400aab6699e4bc8b9d59edc2a2cbae837ff1
fajisnm <87913488+fajisnm@users.noreply.github.com>
2021-08-15 13:01:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer /MusicPlayer
+git clone https://github.com/fajisnm/MusicPlayer /MusicPlayer
 cd /MusicPlayer
 echo "Starting Bot...."
 python3 main.py
